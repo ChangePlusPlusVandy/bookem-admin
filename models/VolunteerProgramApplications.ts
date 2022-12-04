f1c38a5e30f093ac96e005482348ddc5160d750e
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2022-12-04 16:42:11 -0600

    Add the models users, volunteerLogs, volunteerProgramApplications & volunteerPrograms

---
+++ models/VolunteerProgramApplications.ts
@@ -0,0 +1,13 @@
+import mongoose, { SchemaTypes } from 'mongoose';
+
+// VolunteerProgramApplicationSchema describes what our documents should look like in our VolunteerProgramApplications collections
+const VolunteerProgramApplicationSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+  programId: { type: mongoose.Types.ObjectId, ref: 'Program', required: true },
+  formData: { type: mongoose.Schema.Types.Mixed, required: true },
+  }
+);
+
+export default mongoose.models.VolunteerProgramApplication ||
+  mongoose.model('VolunteerProgramApplication', VolunteerProgramApplicationSchema);
---
+++ models/VolunteerPrograms.ts
@@ -0,0 +1,21 @@
+import mongoose, { SchemaTypes } from 'mongoose';
+
+// VolunteerProgramSchema describes what our documents should look like in our VolunteerPrograms collections
+const VolunteerProgramSchema = new mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    schools: [{ type: String, required: false }],
+    programDate: {type: Date, required: true},
+  }, 
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+    collection: 'volunteerPrograms',
+  }
+);
+
+export default mongoose.models.VolunteerProgram ||
+  mongoose.model('VolunteerProgram', VolunteerProgramSchema);
