0333c81b375044b1434f4535734ae301039a3c66
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2022-12-04 17:07:23 -0600

    format prettier

--- models/VolunteerProgramApplications.ts
+++ models/VolunteerProgramApplications.ts
@@ -1,13 +1,14 @@
 import mongoose, { SchemaTypes } from 'mongoose';
 
 // VolunteerProgramApplicationSchema describes what our documents should look like in our VolunteerProgramApplications collections
-const VolunteerProgramApplicationSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+const VolunteerProgramApplicationSchema = new mongoose.Schema({
+  userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
   programId: { type: mongoose.Types.ObjectId, ref: 'Program', required: true },
   formData: { type: mongoose.Schema.Types.Mixed, required: true },
-  }
-);
+});
 
 export default mongoose.models.VolunteerProgramApplication ||
-  mongoose.model('VolunteerProgramApplication', VolunteerProgramApplicationSchema);
+  mongoose.model(
+    'VolunteerProgramApplication',
+    VolunteerProgramApplicationSchema
+  );
--- models/VolunteerPrograms.ts
+++ models/VolunteerPrograms.ts
@@ -3,11 +3,11 @@ import mongoose, { SchemaTypes } from 'mongoose';
 // VolunteerProgramSchema describes what our documents should look like in our VolunteerPrograms collections
 const VolunteerProgramSchema = new mongoose.Schema(
   {
-    name: {type: String, required: true},
-    description: {type: String, required: true},
+    name: { type: String, required: true },
+    description: { type: String, required: true },
     schools: [{ type: String, required: false }],
-    programDate: {type: Date, required: true},
-  }, 
+    programDate: { type: Date, required: true },
+  },
   {
     timestamps: {
       createdAt: 'createdAt',
