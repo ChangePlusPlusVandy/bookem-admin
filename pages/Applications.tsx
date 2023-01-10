d38bc8f9dfe135bde10f4d22c5a84404e2eb1e72
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-01-10 09:43:52 -0600

    Added testing pages

---
+++ pages/Applications.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { VolunteerProgramApplicationData } from 'types/database';
+import mongoose from 'mongoose';
+import { ProgramApplicationTable } from '@/components/ProgramApplicationTable';
+
+/**
+ * Page used to test ProgramApplicationTable component
+ * Restructure is needed in the future to add authentication before displaying the data
+ * @returns
+ */
+export default function applications() {
+  const applications: Array<VolunteerProgramApplicationData> = [
+    {
+      _id: new mongoose.Types.ObjectId(),
+      userId: new mongoose.Types.ObjectId(),
+      user: {
+        _id: new mongoose.Types.ObjectId(),
+        name: 'user1',
+        email: 'user1@bookem.org',
+        password: 'abcdefg',
+        phone: '1234567890',
+        address: 'TN',
+        sourceHeardFrom: 'Change++',
+        ethnicity: 'Asian',
+        gender: 'Male',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      programId: new mongoose.Types.ObjectId(),
+      program: {
+        _id: new mongoose.Types.ObjectId(),
+        name: 'program 1',
+        description: 'program 1',
+        programDate: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      formData: new mongoose.Schema.Types.Mixed(''),
+      createdAt: new Date(),
+    },
+  ];
+
+  return (
+    <>
+      <ProgramApplicationTable
+        headers={['Applicant', 'Email', 'Program', 'Program Date']}
+        applications={applications}
+      />
+    </>
+  );
+}
---
+++ pages/Users.tsx
@@ -0,0 +1,49 @@
+import { UserTable } from '@/components/UserTable';
+import React from 'react';
+import { UserData } from 'types/database';
+import mongoose from 'mongoose';
+
+/**
+ * Page used to test UserTable component
+ * Restructure is needed in the furture to add authentication before displaying the data
+ * @returns
+ */
+export default function Users() {
+  //Dummy users
+  const users: Array<UserData> = [
+    {
+      _id: new mongoose.Types.ObjectId(),
+      name: 'user1',
+      email: 'user1@bookem.org',
+      password: 'abcdefg',
+      phone: '1234567890',
+      address: 'TN',
+      sourceHeardFrom: 'Change++',
+      ethnicity: 'Asian',
+      gender: 'Male',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      _id: new mongoose.Types.ObjectId(),
+      name: 'user2',
+      email: 'user2@bookem.org',
+      password: 'abcdefg',
+      phone: '1234567890',
+      address: 'TN',
+      sourceHeardFrom: 'Change++',
+      ethnicity: 'Asian',
+      gender: 'Female',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+  return (
+    <div>
+      <UserTable
+        headers={['Name', 'Email', 'Phone', 'Address']}
+        users={users}
+      />
+    </div>
+  );
+}
