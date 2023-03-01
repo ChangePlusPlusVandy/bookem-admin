71c0148043f48b794a4cca1de45f25d9ee10ddab
Sebastian <sstull1842@gmail.com>
2023-03-01 14:19:17 -0600

    Added filter for tags

--- components/table/VolunteerTable.tsx
+++ components/table/VolunteerTable.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from 'react';
+import { Input, Table, TableProps } from 'antd';
 
 // VolunteerTable with dummy data (for now)
 
 const VolunteerTable = () => {
-  const columns = [
+  const data = [
+    {
+      key: '1',
+      firstName: 'Davy',
+      lastName: 'Jones',
+      phone: '724-704-1663',
+      email: 'test_user@bookem.org',
+      tags: 'RFR',
+    },
+    {
+      key: '2',
+      firstName: 'Mike',
+      lastName: 'Nesmith',
+      phone: '212-359-0913',
+      email: 'test_user2@bookem.org',
+      tags: 'RIF',
+    },
+  ];
+
+  const columns: any = [
     {
       title: 'First Name',
       dataIndex: 'firstName',
@@ -29,6 +48,18 @@ const VolunteerTable = () => {
       title: 'Tags',
       dataIndex: 'tags',
       key: 'tags',
+      filters: [
+        {
+          text: 'RFR',
+          value: 'RFR',
+        },
+        {
+          text: 'RIF',
+          value: 'RIF',
+        },
+      ],
+      onFilter: (value: string, record: { tags: string }) =>
+        record.tags.startsWith(value),
     },
     {
       title: 'See More',
@@ -37,28 +68,9 @@ const VolunteerTable = () => {
     },
   ];
 
-  const dataSource = [
-    {
-      key: '1',
-      firstName: 'Davy',
-      lastName: 'Jones',
-      phone: '724-704-1663',
-      email: 'test_user@bookem.org',
-      tags: 'RFR',
-    },
-    {
-      key: '2',
-      firstName: 'Mike',
-      lastName: 'Nesmith',
-      phone: '212-359-0913',
-      email: 'test_user2@bookem.org',
-      tags: 'RIF',
-    },
-  ];
-
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns}></Table>
+      <Table dataSource={data} columns={columns}></Table>
     </div>
   );
 };
