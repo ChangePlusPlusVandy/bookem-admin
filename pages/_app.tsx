888784396ee0dd0eddbb76df433746100931003f
Zi Nean Teoh <zinean00@gmail.com>
2023-03-16 21:19:05 -0500

    add Layout and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css';
+import { Container, MainContent } from '@/styles/layout.styles';
+import { SideBar } from '@/components/SideBar';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
@@ -8,7 +10,21 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {session && (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+      {!session && <Component {...pageProps} />}
     </SessionProvider>
   );
 }
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Container>
+      <SideBar />
+      <MainContent>{children}</MainContent>
+    </Container>
+  );
+};
