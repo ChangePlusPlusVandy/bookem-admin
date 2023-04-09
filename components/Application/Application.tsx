b2d487c9e0393b3f2636c111959eb3f6c1680673
leozhang6 <leo.t.zhang@gmail.com>
2023-04-09 13:19:36 -0500

    WIP: 6d5e61c ability to approve and reject applications from volunteer profile page, backend/ basic frontend

--- components/Application/Application.tsx
+++ components/Application/Application.tsx
@@ -2,17 +2,72 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { QueriedVolunteerApplicationData } from 'bookem-shared/src/types/database';
 import { QueriedVolunteerEventData } from 'bookem-shared/src/types/database';
-import {
-  ApproveButton,
-  ApproveText,
-  Container,
-  ItemContainer,
-  PendingContainer,
-  Question,
-  Response,
-  Status,
-  Title,
-} from '@/styles/components/Application/application.styles';
+
+const Container = styled.div`
+  width: 100%;
+  padding: 30px 50px;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: top;
+  overflow-y: auto;
+`;
+
+const Title = styled.p`
+  font-weight: bold;
+  margin: 0px auto;
+  font-size: 20px;
+`;
+const Status = styled.p`
+  margin: 0px auto;
+  font-size: 15px;
+`;
+
+const Question = styled.p`
+  font-size: 15px;
+  font-weight: bold;
+  margin: 0px;
+`;
+
+const Response = styled.p`
+  font-size: 15px;
+  margin: 0px;
+`;
+
+const ItemContainer = styled.div``;
+
+const PendingContainer = styled.div`
+  width: 50%;
+  min-width: 150px;
+  margin: 0px auto;
+  border: black 1px solid;
+  border-radius: 10px;
+  height: 50px;
+  flex: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ApproveText = styled.p`
+  font-size: 15px;
+  font-weight: bold;
+  margin: auto;
+`;
+
+const StatusContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+const ApproveButton = styled.button`
+  flex: none;
+  border: none;
+  margin: auto;
+`;
+
+const DropdownContainer = styled.div``;
+const ToggleDropdown = styled.button``;
 
 const UserApplication = ({
   application,
@@ -23,6 +78,7 @@ const UserApplication = ({
   const [event, setEvent] = useState<QueriedVolunteerEventData>();
   const [approvalStatus, setApprovalStatus] = useState('pending');
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   async function getEvent() {
     try {
       const path = '/api/event/' + application.eventId;
@@ -74,16 +130,11 @@ const UserApplication = ({
   return (
     <Container>
       <Title>Application for {event?.name}</Title>
-      {approvalStatus !== 'pending' && (
-        <Status>Status: {approvalStatus}</Status>
-      )}
-      {approvalStatus === 'pending' && (
-        <PendingContainer>
-          <ApproveText>Approve?</ApproveText>
-          <ApproveButton onClick={handleApproveApplication}></ApproveButton>
-          <ApproveButton onClick={handleRejectApplication}>X</ApproveButton>
-        </PendingContainer>
-      )}
+      <StatusContainer>
+        {!dropdownOpen && <Status>Status: {approvalStatus}</Status>}
+        <ToggleDropdown></ToggleDropdown>
+      </StatusContainer>
+      {dropdownOpen && <DropdownContainer></DropdownContainer>}
       {application.formData.map((item: any) => (
         <ItemContainer key={item.question}>
           <Question>{item.question}</Question>
