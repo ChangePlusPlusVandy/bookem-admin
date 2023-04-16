00228e9b28865366eb6a760f742525cf9a7d62af
leozhang6 <leo.t.zhang@gmail.com>
2023-04-16 15:36:04 -0500

    add settings and types

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,10 +1,22 @@
+import { signOut, useSession } from 'next-auth/react';
 import React from 'react';
 
-const settings = () => {
-  return <div>settings</div>;
+const SettingsPage = () => {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session && (
+        <>
+          <div>You have signed in as {session.user?.email}</div>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </div>
+  );
 };
 
-export default settings;
+export default SettingsPage;
 
 // perform automatic redirection to login page if user not logged in.
 export { getServerSideProps } from '@/lib/getServerSideProps';
