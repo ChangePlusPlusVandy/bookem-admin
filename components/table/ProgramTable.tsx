5a1d329d13636830edfed9836fc3d7b4704aea08
huongthaoh <121412317+huongthaoh@users.noreply.github.com>
2023-11-15 20:08:00 -0600

    add customized filter for program table

--- components/table/ProgramTable.tsx
+++ components/table/ProgramTable.tsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 
 import type { InputRef } from 'antd';
 import { Button, Input, Space, Table } from 'antd';
@@ -14,26 +16,13 @@ import {
 import Image from 'next/image';
 import CreateProgramPopupWindow from '@/components/Forms/CreateProgramPopupWindow';
 
-const SearchOutlined = require('public/arrow-left.png');
-
 interface ProgramRowData {
   key: number;
   programName: string;
   programDesc: string;
 }
 
-const columns: any = [
-  {
-    title: 'Program Name',
-    dataIndex: 'programName',
-    key: 'programName',
-  },
-  {
-    title: 'Description',
-    dataIndex: 'programDesc',
-    key: 'programDesc',
-  },
-];
+type DataIndex = keyof ProgramRowData;
 
 const ProgramTable = () => {
   const [showPopUp, setShowPopUp] = useState(false);
@@ -43,6 +32,128 @@ const ProgramTable = () => {
   ]);
   const [filterTable, setFilterTable] = useState<ProgramRowData[]>([]);
   const [isFiltering, setIsFilter] = useState<boolean>(false);
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): ColumnType<ProgramRowData> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon=""
+            size="small"
+            style={{ width: 90 }}>
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}>
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}>
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined
+        style={{ color: filtered ? '#1677ff' : undefined }}
+        rev={undefined}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: visible => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  const columns: any = [
+    {
+      title: 'Program Name',
+      dataIndex: 'programName',
+      key: 'programName',
+      ...getColumnSearchProps('programName'),
+    },
+    {
+      title: 'Description',
+      dataIndex: 'programDesc',
+      key: 'programDesc',
+    },
+  ];
 
   return (
     <>
