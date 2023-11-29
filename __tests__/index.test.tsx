914dc750769a9356db73159f865c5abbf9dd8695
Harry Huang <jiashu.huang@vanderbilt.edu>
2023-11-28 23:57:29 -0600

    Fix jest

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,23 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import HomePage from '@/pages/index';
-jest.mock('next-auth/react');
+import { useSession } from 'next-auth/react';
 
+// Mock the useSession hook
+jest.mock('next-auth/react', () => {
+  const originalModule = jest.requireActual('next-auth/react');
+  const mockSession = {
+    expires: new Date(Date.now() + 2 * 86400).toISOString(),
+    user: { username: 'Test Admin' },
+  };
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSession: jest.fn(() => {
+      return { data: mockSession, status: 'authenticated' }; // return type is [] in v3 but changed to {} in v4
+    }),
+  };
+});
 describe('Home', () => {
   it('renders correctly', () => {
     // renders the component
