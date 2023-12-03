a431f5326fdca9d738a8d4d9df0e5d3163982040
Manish Acharya <manish.dev2060@gmail.com>
2023-12-03 17:49:55 -0600

    Design Settings page admin table

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { signOut } from 'next-auth/react';
 import styled from 'styled-components';
+import { PageLayout, PageTitle } from '@/styles/table.styles';
+import { Admin } from 'mongodb';
+import AdminTable from '@/components/table/AdminTable';
 
 const MainContainer = styled.div`
   width: 100%;
@@ -9,10 +12,12 @@ const MainContainer = styled.div`
 
 const settings = () => {
   return (
-    <MainContainer>
-      <div>You have signed in</div>
-      <button onClick={() => signOut()}>Sign out</button>
-    </MainContainer>
+    <PageLayout>
+      <PageTitle>Admin Management and Settings</PageTitle>
+      <AdminTable />
+    </PageLayout>
+    /* <div>You have signed in</div>
+      <button onClick={() => signOut()}>Sign out</button> */
   );
 };
 
