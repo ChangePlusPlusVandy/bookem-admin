cfd914975d45edc361c6b8216c1c42d80564cddb
huongthaoh <121412317+huongthaoh@users.noreply.github.com>
2024-01-14 16:37:31 -0600

    add info box

--- components/Forms/TagEventPopupWindow.tsx
+++ components/Forms/TagEventPopupWindow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PopupWindow from '@/components/PopupWindow';
 import Image from 'next/image';
 // import {
@@ -18,6 +18,8 @@ import {
 import { useForm } from 'react-hook-form';
 import {
   InfoHeader,
+  InfoList,
+  InfoListItem,
   InfoSubheader,
   InfoTextBox,
   MiddleContainer,
@@ -37,27 +39,47 @@ const TagEventPopupWindow = ({
 }) => {
   const { handleSubmit } = useForm({});
   const onSubmit = (data: any) => {};
+  const [showInfo, setShowInfo]  = useState(false);
+
   return (
     <PopupWindow hidePopup={() => setShowPopup(false)}>
       <TagEventContainer>
         <TagEventHeader>Manage tags</TagEventHeader>
         <TagBodyContainer>
+          {showInfo && (            
           <TagInfoContainer>
             <InfoTextBox>
               <InfoHeader>Tips</InfoHeader>
               <InfoSubheader>How to use this page?</InfoSubheader>
+              <InfoList>
+                <InfoListItem>Tags are categories of events.</InfoListItem>
+                <InfoListItem>
+                  To create new tags: enter tag names and click check mark.
+                </InfoListItem>
+                <InfoListItem>
+                  To edit existing tags: double click a tag and modify.
+                </InfoListItem>
+                <InfoListItem>
+                  To delete a tag: hover over a tag and click the trash icon.
+                </InfoListItem>
+              </InfoList>
             </InfoTextBox>
           </TagInfoContainer>
+
+            )}
+          
 
           <MiddleContainer>
             <SearchContainer>
               <Image
+                onClick = {() => setShowInfo(showInfo => !showInfo)}
                 src="/./event/Info.png"
                 alt="info button"
                 width="20"
                 height="20"
+                style = {{cursor: "pointer"}}
               />
-              <SearchInput></SearchInput>
+              <SearchInput />
               <Image
                 src="/./event/CheckSubmit.png"
                 alt="submit tag"
--- styles/components/Event/eventTagPopupWindow.styles.tsx
+++ styles/components/Event/eventTagPopupWindow.styles.tsx
@@ -5,7 +5,8 @@ export const TagEventContainer = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: start;
+  /* justify-content: start; */
+  justify-content: center;
   align-items: center;
   padding: 30px 20px 50px 20px;
 `;
@@ -15,46 +16,58 @@ export const TagEventHeader = styled.h2`
 `;
 
 export const TagBodyContainer = styled.div`
+  position: relative;
   display: flex;
   align-items: start;
-  justify-content: start;
+  /* justify-content: start; */
+  justify-content: center;
   height: 100%;
   width: 100%;
-  padding: 1
 `;
 
 export const TagInfoContainer = styled.div`
   /* flex-direction: column; */
   display: flex;
-  width: 25%;
+  width: 20%;
   height: 100%;
   /* background-color: lightsalmon; */
   padding: 0 10px;
+  position: absolute;
+  left: 5%;
+  /* margin-right: 20px; */
   /* margin-top: 20px; */
 `;
 
 export const InfoTextBox = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 5%;
-  width: 100%;
+  padding: 5% 5% 5% 0;
+  width: 90%;
   height: 100%;
   border: 1px black solid;
   border-radius: 15px;
-`
+`;
 export const InfoHeader = styled.p`
   font-size: ${props => props.theme.fontSizes.SMALL};
   font-weight: 700;
   text-align: center;
   margin: 0;
-`
+`;
 
 export const InfoSubheader = styled.p`
   font-size: ${props => props.theme.fontSizes.EXTRA_SMALL};
   font-weight: 400;
   text-decoration: underline;
   text-align: center;
-`
+`;
+export const InfoList = styled.ul`
+  height: 100%;
+`;
+
+export const InfoListItem = styled.li`
+  margin-bottom: 5px;
+  line-height: 1.5em;
+`;
 
 export const MiddleContainer = styled.div`
   display: flex;
