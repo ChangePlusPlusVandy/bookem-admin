56ceba47fa51b6901bae948232dc3098987fcc41
Harry Huang <jiashu.huang@vanderbilt.edu>
2024-01-16 12:22:44 -0600

    Add styledComponents support in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['bookem-shared'],
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
