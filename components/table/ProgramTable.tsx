65931377dd57771e9098fc870472c1a10b465127
Ellie Li <ellieli@Ellies-MacBook-Pro.local>
2024-01-21 14:13:18 -0600

    pulling

--- components/table/ProgramTable.tsx
+++ components/table/ProgramTable.tsx
@@ -1,210 +1,258 @@
-import React, { useRef, useState } from 'react';
-import Highlighter from 'react-highlight-words';
-import { SearchOutlined } from '@ant-design/icons';
+// import React, { useRef, useState } from 'react';
+// import Highlighter from 'react-highlight-words';
+// import { SearchOutlined } from '@ant-design/icons';
 
-import type { InputRef } from 'antd';
-import { Button, Input, Space, Table } from 'antd';
-import type { ColumnType, ColumnsType } from 'antd/es/table';
-import type { FilterConfirmProps } from 'antd/es/table/interface';
-import {
-  SearchContainter,
-  StyledTable,
-  TableButton,
-  TableContainer,
-} from '@/styles/table.styles';
+// import { Button, Input, Space, Tag, Table, TableProps, InputRef } from 'antd';
+// import type { ColumnType, ColumnsType } from 'antd/es/table';
+// import type { FilterConfirmProps } from 'antd/es/table/interface';
+// import {
+//   SearchContainter,
+//   StyledTable,
+//   TableButton,
+//   TableContainer,
+// } from '@/styles/table.styles';
 
-import Image from 'next/image';
+// import { ObjectId } from 'mongodb';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import CreateProgramPopupWindow from '@/components/Forms/CreateProgramPopupWindow';
+
+// interface ProgramRowData {
+//   key: number;
+//   programName: string;
+//   programDesc: string;
+// }
+
+// type DataIndex = keyof ProgramRowData;
+
+// const fetcher = (url: string) => fetch(url).then(res => res.json());
+
+// const ProgramTable = () => {
+//   const [showPopUp, setShowPopUp] = useState(false);
+//   const [dataForTable, setDataForTable] = useState<ProgramRowData[]>([
+//     {
+//       key: 1,
+//       programName: 'test',
+//       programDesc: 'test program',
+//     },
+//     {
+//       key: 2,
+//       programName: 'test 2',
+//       programDesc: 'test program 2',
+//     },
+//   ]);
+//   const [filterTable, setFilterTable] = useState<ProgramRowData[]>([]);
+//   const [isFiltering, setIsFilter] = useState<boolean>(false);
+
+//   const [searchText, setSearchText] = useState('');
+//   const [searchedColumn, setSearchedColumn] = useState('');
+//   const searchInput = useRef<InputRef>(null);
+
+//   const handleSearch = (
+//     selectedKeys: string[],
+//     confirm: (param?: FilterConfirmProps) => void,
+//     dataIndex: DataIndex
+//   ) => {
+//     confirm();
+//     setSearchText(selectedKeys[0]);
+//     setSearchedColumn(dataIndex);
+//   };
+
+//   const handleReset = (clearFilters: () => void) => {
+//     clearFilters();
+//     setSearchText('');
+//   };
+
+//   const getColumnSearchProps = (
+//     dataIndex: DataIndex
+//   ): ColumnType<ProgramRowData> => ({
+//     filterDropdown: ({
+//       setSelectedKeys,
+//       selectedKeys,
+//       confirm,
+//       clearFilters,
+//       close,
+//     }) => (
+//       <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+//         <Input
+//           ref={searchInput}
+//           placeholder={`Search ${dataIndex}`}
+//           value={selectedKeys[0]}
+//           onChange={e =>
+//             setSelectedKeys(e.target.value ? [e.target.value] : [])
+//           }
+//           onPressEnter={() =>
+//             handleSearch(selectedKeys as string[], confirm, dataIndex)
+//           }
+//           style={{ marginBottom: 8, display: 'block' }}
+//         />
+//         <Space>
+//           <Button
+//             type="primary"
+//             onClick={() =>
+//               handleSearch(selectedKeys as string[], confirm, dataIndex)
+//             }
+//             icon=""
+//             size="small"
+//             style={{ width: 90 }}>
+//             Search
+//           </Button>
+//           <Button
+//             onClick={() => clearFilters && handleReset(clearFilters)}
+//             size="small"
+//             style={{ width: 90 }}>
+//             Reset
+//           </Button>
+//           <Button
+//             type="link"
+//             size="small"
+//             onClick={() => {
+//               confirm({ closeDropdown: false });
+//               setSearchText((selectedKeys as string[])[0]);
+//               setSearchedColumn(dataIndex);
+//             }}>
+//             Filter
+//           </Button>
+//           <Button
+//             type="link"
+//             size="small"
+//             onClick={() => {
+//               close();
+//             }}>
+//             close
+//           </Button>
+//         </Space>
+//       </div>
+//     ),
+//     filterIcon: (filtered: boolean) => (
+//       <SearchOutlined
+//         style={{ color: filtered ? '#1677ff' : undefined }}
+//         rev={undefined}
+//       />
+//     ),
+//     onFilter: (value, record) =>
+//       record[dataIndex]
+//         .toString()
+//         .toLowerCase()
+//         .includes((value as string).toLowerCase()),
+//     onFilterDropdownOpenChange: visible => {
+//       if (visible) {
+//         setTimeout(() => searchInput.current?.select(), 100);
+//       }
+//     },
+//     render: text =>
+//       searchedColumn === dataIndex ? (
+//         <Highlighter
+//           highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+//           searchWords={[searchText]}
+//           autoEscape
+//           textToHighlight={text ? text.toString() : ''}
+//         />
+//       ) : (
+//         text
+//       ),
+//   });
+//   const columns: any = [
+//     {
+//       title: 'Program Name',
+//       dataIndex: 'programName',
+//       key: 'programName',
+//       ...getColumnSearchProps('programName'),
+//     },
+//     {
+//       title: 'Description',
+//       dataIndex: 'programDesc',
+//       key: 'programDesc',
+//     },
+//     {
+//       title: 'Events',
+//       dataIndex: 'programEvent',
+//       key: 'programEvent',
+//     },
+//     {
+//       title: 'Volunteers',
+//       dataIndex: 'programVolunteer',
+//       key: 'programVolunteer',
+//     },
+//   ];
+
+//   return (
+//     <>
+//       {showPopUp && <CreateProgramPopupWindow setShowPopup={setShowPopUp} />}
+//       <div>
+//         <SearchContainter>
+//           <TableButton onClick={() => setShowPopUp(prev => !prev)}>
+//             <Image
+//               src="/table/addbutton.png"
+//               alt=""
+//               width={32}
+//               height={32}
+//               style={{ marginLeft: 150 }}
+//             />
+//           </TableButton>
+//         </SearchContainter>
+//         <TableContainer>
+//           <div id="table-container">
+//             <StyledTable
+//               dataSource={isFiltering ? filterTable : dataForTable}
+//               columns={columns}
+//               pagination={false}
+//               scroll={{ y: 550 }}
+//             />
+//           </div>
+//         </TableContainer>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ProgramTable;
+
+import React, { useState } from 'react';
+import { Table } from 'antd';
+import useSWR from 'swr';
 import CreateProgramPopupWindow from '@/components/Forms/CreateProgramPopupWindow';
 
-interface ProgramRowData {
-  key: number;
-  programName: string;
-  programDesc: string;
-  programEvent: string;
-  programVolunteer: string;
+// Define the structure for the program data based on the API response
+interface ProgramData {
+  // Update these fields based on your actual data structure
+  _id: string;
+  name: string;
+  description: string;
+  // Add other fields as needed
 }
 
-type DataIndex = keyof ProgramRowData;
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 const ProgramTable = () => {
-  const [showPopUp, setShowPopUp] = useState(false);
-  const [dataForTable, setDataForTable] = useState<ProgramRowData[]>([
-    {
-      key: 1,
-      programName: 'test',
-      programDesc: 'test program',
-      programEvent: 'test programEvent',
-      programVolunteer: 'Harry',
-    },
-    {
-      key: 2,
-      programName: 'test 2',
-      programDesc: 'test program 2',
-      programEvent: 'test programEvent 2',
-      programVolunteer: 'Jessie',
-    },
-  ]);
-  const [filterTable, setFilterTable] = useState<ProgramRowData[]>([]);
-  const [isFiltering, setIsFilter] = useState<boolean>(false);
+  const { data, error } = useSWR<ProgramData[]>('/api/program/', fetcher);
+  const [showPopup, setShowPopup] = useState(false);
 
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
+  if (error) return <div>Failed to load programs</div>;
+  if (!data) return <div>Loading...</div>;
 
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters();
-    setSearchText('');
-  };
-
-  const getColumnSearchProps = (
-    dataIndex: DataIndex
-  ): ColumnType<ProgramRowData> => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      close,
-    }) => (
-      <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={e =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() =>
-            handleSearch(selectedKeys as string[], confirm, dataIndex)
-          }
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() =>
-              handleSearch(selectedKeys as string[], confirm, dataIndex)
-            }
-            icon=""
-            size="small"
-            style={{ width: 90 }}>
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}>
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText((selectedKeys as string[])[0]);
-              setSearchedColumn(dataIndex);
-            }}>
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}>
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined
-        style={{ color: filtered ? '#1677ff' : undefined }}
-        rev={undefined}
-      />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: visible => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: text =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      ),
-  });
-  const columns: any = [
+  const columns = [
     {
       title: 'Program Name',
-      dataIndex: 'programName',
-      key: 'programName',
-      ...getColumnSearchProps('programName'),
+      dataIndex: 'name', // This should match the field in your API response
+      key: 'name',
     },
     {
       title: 'Description',
-      dataIndex: 'programDesc',
-      key: 'programDesc',
+      dataIndex: 'description', // This should match the field in your API response
+      key: 'description',
     },
-    {
-      title: 'Events',
-      dataIndex: 'programEvent',
-      key: 'programEvent',
-    },
-    {
-      title: 'Volunteers',
-      dataIndex: 'programVolunteer',
-      key: 'programVolunteer',
-    },
+    // ... other columns as needed
   ];
 
   return (
     <>
-      {showPopUp && <CreateProgramPopupWindow setShowPopup={setShowPopUp} />}
-      <div>
-        <SearchContainter>
-          <TableButton onClick={() => setShowPopUp(prev => !prev)}>
-            <Image
-              src="/table/addbutton.png"
-              alt=""
-              width={32}
-              height={32}
-              style={{ marginLeft: 150 }}
-            />
-          </TableButton>
-        </SearchContainter>
-        <TableContainer>
-          <div id="table-container">
-            <StyledTable
-              dataSource={isFiltering ? filterTable : dataForTable}
-              columns={columns}
-              pagination={false}
-              scroll={{ y: 550 }}
-            />
-          </div>
-        </TableContainer>
-      </div>
+      {showPopup && <CreateProgramPopupWindow setShowPopup={setShowPopup} />}
+      <Table
+        dataSource={data}
+        columns={columns}
+        rowKey={record => record._id} // Use the unique identifier from your data
+      />
     </>
   );
 };
---
+++ pages/api/program/[id].ts
@@ -0,0 +1,147 @@
+import dbConnect from '@/lib/dbConnect';
+import Users from 'bookem-shared/src/models/Users';
+import VolunteerPrograms from 'bookem-shared/src/models/VolunteerPrograms';
+import Tags from 'bookem-shared/src/models/Tags';
+import { ObjectId } from 'mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Get session user
+  const session = await getServerSession(req, res, authOptions);
+
+  // Get request parameter
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  switch (method) {
+    /**
+     * @route GET /api/program/[id]
+     * @desc Get program by id
+     * @req program id, user in session
+     * @res QueriedVolunteerProgramData
+     */
+    case 'GET':
+      try {
+        await dbConnect();
+
+        if (!id) return res.status(400).json({ message: 'Missing id' });
+
+        // check if id is a valid mongoose id
+        if (!ObjectId.isValid(id as string))
+          return res.status(400).json({ message: 'Invalid id' });
+
+        // TODO: remove this after development
+        await Tags.find({});
+
+        // query program and populate fields with mongoose refs
+        const program = await VolunteerPrograms.findById(id)
+          .populate({ path: 'program' })
+          .exec();
+
+        // if program is not found
+        if (!program)
+          return res.status(400).json({ message: 'Program not found' });
+
+        return res.status(200).json(program);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error });
+      }
+      break;
+
+    /**
+     * @route POST /api/program/[id]
+     * @desc Signup/Unsignup the user
+     * @req program id, user in session
+     * @res Success message
+     */
+    case 'POST':
+      try {
+        await dbConnect();
+
+        // Query program
+        const program = await VolunteerPrograms.findById(id);
+
+        // Query logged in user
+        const user = await Users.findById(session.user._id);
+
+        // Find the index of logged in user in program.volunteers
+        const userIndex = program.volunteers.indexOf(user._id);
+
+        // Find the index of program in user.programs
+        const programIndex = user.programs.indexOf(program._id);
+
+        if (userIndex === -1 && programIndex === -1) {
+          // Register to the program
+          program.volunteers.unshift(user._id);
+          user.programs.unshift(program._id);
+        } else if (userIndex === -1 || programIndex === -1) {
+          throw new Error('Inconsistency between collections!');
+        } else {
+          // Unregister
+          // Remove the user and program
+          program.volunteers.splice(userIndex, 1);
+          user.programs.splice(programIndex, 1);
+        }
+
+        // Resave both document
+        await user.save();
+        await program.save();
+
+        return res.status(200).json('Register Success');
+      } catch (error: any) {
+        res.status(500).json({ message: error.message });
+        console.error(error);
+      }
+
+      break;
+
+    /**
+     * @route PATCH /api/program/[id]
+     * @desc Update the program by id
+     * @req program id, user in session
+     * @res Success message
+     */
+    case 'PATCH':
+      try {
+        // connect to db
+        await dbConnect();
+
+        if (!id) return res.status(400).json({ message: 'Missing id' });
+
+        // check if id is a valid mongoose id
+        if (!ObjectId.isValid(id as string))
+          return res.status(400).json({ message: 'Invalid id' });
+
+        // validate that req.body follows the VolunteerProgramSchema
+
+        // update program based on input
+        const program = await VolunteerPrograms.findByIdAndUpdate(id, req.body);
+        console.log('before update: ', program);
+        console.log('after update: ', req.body);
+
+        // if program is not found
+        if (!program)
+          return res.status(400).json({ message: 'Program not found' });
+
+        return res.status(200).json('Update Success');
+      } catch (error: any) {
+        res.status(500).json({ message: error.message });
+        console.error(error);
+      }
+
+      break;
+    // case 'PUT':
+    // case 'DELETE':
+    // default:
+    // res.setHeader('Allow', ['GET', 'PUT', 'DELETE', 'POST']);
+    // res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
