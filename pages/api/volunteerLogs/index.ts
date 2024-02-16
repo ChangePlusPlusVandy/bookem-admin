a920151f22e797fc29949a7b196d92bfa70b4d53
jjessieyang <55462083+jjessieyang@users.noreply.github.com>
2024-02-15 18:32:14 -0600

    Create a new volunteer log

--- pages/api/volunteerLogs/index.ts
+++ pages/api/volunteerLogs/index.ts
@@ -4,12 +4,42 @@ import dbConnect from '@/lib/dbConnect';
 
 import VolunteerLogs from 'bookem-shared/src/models/VolunteerLogs';
 import { QueriedVolunteerLogData } from 'bookem-shared/src/types/database';
+import { VolunteerLogData } from 'bookem-shared/src/types/database';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
-) {
-  const { userid } = req.query;
+) {  // check that user is authenticated
+  const validateData = (volunteerLog: VolunteerLogData) => {
+    if (!volunteerLog.eventId) {
+      res.status(400).json({
+        message: 'You forgot to select an event.',
+      });
+      return;
+    }
+
+    if (!volunteerLog.hours) {
+      res
+        .status(400)
+        .json({ message: 'You forgot to fill in number of hours.' });
+      return;
+    }
+
+    if (!volunteerLog.numBooks) {
+      res
+        .status(400)
+        .json({ message: 'You forgot to fill in number of books donated' });
+      return;
+    }
+
+    if (!volunteerLog.date) {
+      res.status(400).json({ message: 'You forgot to fill in date' });
+      return;
+    }
+  };
+
   switch (req.method) {
     case 'GET':
       try {
@@ -26,7 +56,32 @@ export default async function handler(
       break;
     
     case 'POST':
-      
+      try {
+        // connect to our database
+        await dbConnect();
+
+        // start a try catch block to catch any errors in parsing the request body
+        const volunteerLog = req.body as VolunteerLogData;
+
+        validateData(volunteerLog);
+
+        // construct the object we want to insert into our database
+        await VolunteerLogs.insertMany(volunteerLog);
+
+        // return the result of the action
+        res.status(200).json({
+          message: 'Successfully Logged hours',
+        });
+      } catch (e) {
+        // if there is an error, print and return the error
+        console.error('An error has occurred in volunteerLogs/create.ts', e);
+        res.status(500).json({
+          error:
+            'Sorry, an error occurred while connecting/inserting to the database: ',
+          message: '' + e,
+        });
+      }
+      break;
 
     default:
       res.status(405).json({
