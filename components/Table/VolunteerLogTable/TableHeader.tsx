1ba46d29ae098053e814534373cccd8debd4b8e0
Harry Huang <jiashu.huang@vanderbilt.edu>
2024-04-14 18:26:52 -0500

    Finish setting up options

--- components/Table/VolunteerLogTable/TableHeader.tsx
+++ components/Table/VolunteerLogTable/TableHeader.tsx
@@ -3,27 +3,31 @@ import {
   SelectContainer,
 } from '@/styles/components/Table/VolunteerLogTable.styles';
 import { Button, Select } from 'antd';
-import React from 'react';
+import { VolunteerLogStatus } from 'bookem-shared/src/types/database';
+import React, { useEffect, useState } from 'react';
 
 const TableHeader = () => {
   const handleChange = (value: string) => {
     console.log(`selected ${value}`);
   };
+
+  const [statusOptions, setStatusOptions] = useState<any[]>([]);
+  useEffect(() => {
+    setStatusOptions(
+      Object.values(VolunteerLogStatus).map(value => ({ value, label: value }))
+    );
+  }, []);
+
   return (
     <>
       <HeaderContainer>
         <SelectContainer>
           <span style={{ whiteSpace: 'nowrap' }}>Choose status: </span>
           <Select
-            defaultValue="lucy"
+            defaultValue="pending"
             style={{ width: 120 }}
             onChange={handleChange}
-            options={[
-              { value: 'jack', label: 'Jack' },
-              { value: 'lucy', label: 'Lucy' },
-              { value: 'Yiminghe', label: 'yiminghe' },
-              { value: 'disabled', label: 'Disabled', disabled: true },
-            ]}
+            options={statusOptions}
           />
         </SelectContainer>
         <Button style={{ marginRight: '30px' }}>Approve</Button>
