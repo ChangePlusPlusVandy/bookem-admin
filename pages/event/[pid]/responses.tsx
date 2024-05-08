99101e7b003ca9a13c9facb88ba673032c1d13ff
Harry Huang <jiashu.huang@vanderbilt.edu>
2024-05-08 09:15:28 +0800

    Add a title

--- pages/event/[pid]/responses.tsx
+++ pages/event/[pid]/responses.tsx
@@ -1,19 +1,25 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import ApplicationTable from '@/components/Table/ApplicationTable/ApplicationTable';
+import { PageTitle, PageLayout } from '@/styles/table.styles';
 
 export default function responses() {
   return (
     <>
-      <Link
-        href="#"
-        onClick={() => window.history.back()}
-        style={{
-          margin: '0 0 0 20px',
-        }}>
-        <Image src="/event/arrow-left.svg" alt="" width={48} height={48} />
-      </Link>
-      <ApplicationTable />
+      <PageLayout>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Link
+            href="#"
+            onClick={() => window.history.back()}
+            style={{
+              margin: '0 20px 0 0',
+            }}>
+            <Image src="/event/arrow-left.svg" alt="" width={48} height={48} />
+          </Link>
+          <PageTitle>Event Management</PageTitle>
+        </div>
+        <ApplicationTable />
+      </PageLayout>
     </>
   );
 }
